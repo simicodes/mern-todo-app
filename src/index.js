f1beba1ebb1bf8b5fe4d01872b667c2c1eb44a7f
simicodes <simeonoffiong@gmail.com>
2022-09-02 12:27:07 +0100

    Integrated redux, react-redux and redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./store/reducers/rootReducer";
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-

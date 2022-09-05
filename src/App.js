5477c82bf2c3ed06d7d0917f6e4721a12743dceb
simicodes <simeonoffiong@gmail.com>
2022-09-05 13:30:43 +0100

    Done with loadUser functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Container } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { loadUser } from "./store/actions/authActions";
 
 import Todos from "./components/todos/Todos";
 import SignIn from "./components/auth/SignIn";
@@ -16,6 +19,10 @@ const useStyles = makeStyles({
 });
 function App() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadUser());
+  }, [dispatch]);
   return (
     <>
       <BrowserRouter>
